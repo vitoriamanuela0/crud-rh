9eddec49153ee175ba76462fdc63807af1e3f57e
vitoriamanuela0 <contatodavitoria@gmail.com>
2025-02-05 18:52:41 -0300

    Projeto inicializado ⚙️

---
+++ src/main/java/com/vmrh/VmrhApplication.java
@@ -0,0 +1,13 @@
+package com.vmrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmrhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmrhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vmrh/VmrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmrhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
